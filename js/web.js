7ad63b250bb5540620c10b654d8a0af13aebf2da
Shikhajaiswar <shikhajaiswar23@gmail.com>
2022-02-04 17:02:18 +0530

    change

--- js/web.js
+++ js/web.js
@@ -2,15 +2,13 @@ burgur = document.querySelector('.burgur');
 navbar = document.querySelector('.navbar');
 nav_bar = document.querySelector('.nav-bar');
 
-// let links = document.querySelectorAll('.links')
-
 burgur.addEventListener('click',()=>{
     
     // rightnav.classList.toggle('v-nav-resp');
-    nav_bar.classList.toggle('v-class-resp');
+    // nav_bar.classList.toggle('v-class-resp');
     navbar.classList.toggle('h-class-resp');
 
-})
+});
 
 
 
